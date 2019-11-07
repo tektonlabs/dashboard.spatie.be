9d131e8501cfa71591cf42ae409d8115335e995f
leila-tk <leila.lazo@tektonlabs.com>
2019-11-07 12:58:57 -0500

    dashboard with team members component

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,10 +9,11 @@
         <time-weather position="a1:a5" date-format="dddd MM/DD" time-zone="America/Lima" weather-city="Lima" src="{{asset('images/logo.png')}}"></time-weather>
         <time-zone position="b1:b5" date-format="ddd MM/DD" time-format="hh:mm a"></time-zone>
         <ontime position="c1:c10" calendar-title="👩‍💻 Ontime 👨‍💻"></ontime>
-        <calendar position="d1:d10" calendar-summary="TK Events" calendar-title="Events👯"></calendar>
-        <calendar position="e1:e10" calendar-summary="TK Birthdays" calendar-title="Birthdays🎂"></calendar>
+        <calendar position="d1:d5" calendar-summary="TK Events" calendar-title="Events👯"></calendar>
+        <calendar position="e1:e5" calendar-summary="TK Birthdays" calendar-title="Birthdays🎂"></calendar>
         <internet-connection position="e10:e6"></internet-connection>
-        <help position="a6:b10"></help>
+        <help position="d6:e10"></help>
+        <team-members position="a6:b10"></team-members>
     </dashboard>
 </div>
 @endsection
