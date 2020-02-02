fb6351133be04de325d786479229704993b5b3a6
Joca <jpereyraleon@gmail.com>
2020-02-01 22:39:16 -0500

    Update members status (#13)
    * read from spreadsheet
    
    * retrieve values from spreadsheet

--- app/Console/Components/TeamMembers/FetchTeamMembersFromSpreadsheet.php
+++ app/Console/Components/TeamMembers/FetchTeamMembersFromSpreadsheet.php
@@ -2,10 +2,10 @@
 
 namespace App\Console\Components\TeamMembers;
 
+use App\Events\TeamMembers\MembersFetched;
 use Illuminate\Console\Command;
 use Google_Client;
 use Google_Service_Sheets;
-use Google_Service_Drive;
 
 class FetchTeamMembersFromSpreadsheet extends Command
 {
@@ -15,115 +15,36 @@ class FetchTeamMembersFromSpreadsheet extends Command
 
     public function handle()
     {
-        $client = $this->getClient();
-        // $service = new Google_Service_Sheets($client);
-        // $spreadsheetId = config('services.team-members.spreadsheet_id');
-        // $range = 'Sheet1!A2:B1000';
-        // $queryParameters = [
-        //     'majorDimension' => 'COLUMNS',
-        // ];
-        // $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-        // $values = $response;
-        // $this->info('Fetching team members events...');
-        dd($client);
+        $reader = $this->getSpreadsheetService();
+        $spreadsheetId = config('services.team-members.spreadsheet_id');
+        $collaborators = collect($reader->spreadsheets_values->get($spreadsheetId, 'Data'));
+        $clients = $collaborators->shift();
+        $members = collect($clients)->map(function($client) {
+            return ['client' => $client];
+        })->toArray();
+        $collaborators->each(function($row) use(&$members) {
+            foreach($row as $index => $value) {
+                if($value !== "") {
+                    $members[$index]['members'][] = $value;
+                }
+            }
+        });
 
-        // $calendarId = $calendarId ?? config('google-calendar.calendar_id');
-        // $calendarService = GoogleCalendarFactory::createForCalendarId($calendarId)->getService();
-        // $now = Carbon::now();
-        // $startDate = $now->startOfMonth();
-        // $endDate = $now->endOfMonth();
-
-
-        // foreach ($calendarService->calendarList->listCalendarList(['minAccessRole' => 'owner']) as $calendarListItem) {
-        //     $calendarId = $calendarListItem->getId();
-
-        //     $allEvents = collect(Event::get($startDate, $endDate, $queryParameters, $calendarId))
-        //         ->map(function (GoogleCalendarEvent $event) {
-        //             $sortDate = $event->getSortDate();
-        //             $date =  $event->start->date
-        //                 ? Carbon::createFromFormat('Y-m-d', $event->start->date)
-        //                 : Carbon::parse($event->start->dateTime);
-
-        //             $attendees = collect($event->attendees)->map(function($element) {
-        //                 $username = explode('@', $element->email);
-
-        //                 return [
-        //                     'name' => ucwords((implode(' ', explode('.', $username[0]))))
-        //                 ];
-        //             });
-
-        //             return [
-        //                 'attendees' => $attendees,
-        //                 'description' => $event->description,
-        //                 'date' => $date->format('Y-m-d'),
-        //                 'name' => $event->name,
-        //             ];
-        //         })
-        //         ->toArray();
-
-        //     $events[$calendarListItem->getSummary()] = [
-        //         'calendarName' => $calendarListItem->getSummary(),
-        //         'events' => $allEvents,
-        //     ];
-        // }
-
-        // event(new EventsFetched($events));
-
+        $this->info('Fetching team members events...');
+        event(new MembersFetched($members));
         $this->info('All done!');
     }
 
     /**
      * Returns an authorized API client.
-     * @return Google_Client the authorized client object
+     * @return Google_Service_Sheets the authorized client object
      */
-    function getClient()
+    function getSpreadsheetService()
     {
         $client = new Google_Client();
-
-        $client->setApplicationName('Google Sheets API PHP Quickstart');
+        $client->setApplicationName('Dashboard Spreadsheet Reader');
         $client->setScopes(Google_Service_Sheets::SPREADSHEETS_READONLY);
         $client->setAuthConfig(config('google-calendar.service_account_credentials_json'));
-
-        $client->setAccessType('offline');
-        $client->setPrompt('select_account consent');
-
-        // Load previously authorized token from a file, if it exists.
-        // The file token.json stores the user's access and refresh tokens, and is
-        // created automatically when the authorization flow completes for the first
-        // time.
-        $tokenPath = 'token.json';
-        if (file_exists($tokenPath)) {
-            $accessToken = json_decode(file_get_contents($tokenPath), true);
-            $client->setAccessToken($accessToken);
-        }
-
-        // If there is no previous token or it's expired.
-        if ($client->isAccessTokenExpired()) {
-            // Refresh the token if possible, else fetch a new one.
-            if ($client->getRefreshToken()) {
-                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
-            } else {
-                // Request authorization from the user.
-                $authUrl = $client->createAuthUrl();
-                printf("Open the following link in your browser:\n%s\n", $authUrl);
-                print 'Enter verification code: ';
-                $authCode = trim(fgets(STDIN));
-
-                // Exchange authorization code for an access token.
-                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
-                $client->setAccessToken($accessToken);
-
-                // Check to see if there was an error.
-                if (array_key_exists('error', $accessToken)) {
-                    throw new Exception(join(', ', $accessToken));
-                }
-            }
-            // Save the token to a file.
-            if (!file_exists(dirname($tokenPath))) {
-                mkdir(dirname($tokenPath), 0700, true);
-            }
-            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
-        }
-        return $client;
+        return new Google_Service_Sheets($client);;
     }
 }
